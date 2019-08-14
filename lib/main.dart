e7607f93f34e3c3d0475a6fd74a8f71de225a969
hisnameispum <sirasitxp@gmail.com>
2019-08-14 12:59:42 -0700

    Added call to action buttons

--- lib/main.dart
+++ lib/main.dart
@@ -11,35 +11,88 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/pum.jpg'),
-              ),
-              SizedBox(
-                width: double.infinity,
-              ),
-              Text(
-                'Sirasit T',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+          child: Container(
+            margin: EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 0.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/pum.jpg'),
                 ),
-              ),
-              Text(
-                'Tech Entrepreneur',
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontFamily: 'SourceSansPro',
-                  color: Colors.white,
+                SizedBox(
+                  width: double.infinity,
                 ),
-              )
-            ],
+                Text(
+                  'Sirasit T',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                Text(
+                  'TECH ENTREPRENEUR',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'SourceSansPro',
+                      color: Colors.teal[100],
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 2.5),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                        size: 20.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        '+1 415-608-0084',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 20.0,
+                          fontFamily: 'SourceSansPro',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 0.0),
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.mail,
+                        color: Colors.teal,
+                        size: 20.0,
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                        'SIRASITXP@GMAIL.COM',
+                        style: TextStyle(
+                          color: Colors.teal[900],
+                          fontSize: 20.0,
+                          fontFamily: 'SourceSansPro',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
